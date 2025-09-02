5f01f2dd7e57bfc2fef9da81922536962c3086f5
YuSung Han <hsnyus090406@gmail.com>
2025-09-02 23:13:41 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore, Timestamp } from "firebase/firestore";
 import { getDatabase, ref, push, set, onValue, update, remove } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlaEXQp4cMSWbYfJ-d6Gg07Ge9Q0VVjao",
-    authDomain: "fir-dlqkr.firebaseapp.com",
-    projectId: "fir-dlqkr",
-    storageBucket: "fir-dlqkr.firebasestorage.app",
-    messagingSenderId: "818684156502",
-    appId: "1:818684156502:web:de8c709e107671a43079f7"
+    apiKey: {test},
+    authDomain: {test},
+    projectId: {test},
+    storageBucket: {test},
+    messagingSenderId: {test},
+    appId: {test}
   };
   console.log('firebaseConfig');
 
@@ -18,4 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 export { Timestamp };
+
 export { ref, push, set, onValue, update, remove };
